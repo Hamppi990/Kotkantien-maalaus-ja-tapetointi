344716b61eeae06bedf457fbae87cc528e5d7a6f
Joonas <joonas.juntunen19@gmail.com>
2024-06-19 14:12:10 +0300

    erilliset sivut ja täydennys

--- javascript.js
+++
@@ -1,9 +0,0 @@
-function changeDivColor(color, divId) {
-    const testArea = document.getElementById(divId);
-    testArea.style.backgroundColor = color;
-}
-
-function resetColor(areaId) {
-    document.getElementById(areaId).style.backgroundColor = '';
-}
-
---
+++ javascript/javascript.js
@@ -0,0 +1,49 @@
+function changeDivColor(color, divId) {
+    const testArea = document.getElementById(divId);
+    testArea.style.backgroundColor = color;
+}
+
+function resetColor(areaId) {
+    document.getElementById(areaId).style.backgroundColor = '';
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const images = document.querySelectorAll('.img-fluid');
+    const modal = new bootstrap.Modal(document.getElementById('imageModal'));
+    const modalImage = document.getElementById('modalImage');
+
+    images.forEach(image => {
+        image.addEventListener('click', function() {
+            modalImage.src = this.src;
+            modal.show();
+        });
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('contactForm');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const name = form.querySelector('#name').value.trim();
+        const email = form.querySelector('#email').value.trim();
+        const message = form.querySelector('#message').value.trim();
+
+        if (name === '' || email === '' || message === '') {
+            alert('Täytä kaikki pakolliset kentät.');
+            return;
+        }
+        form.reset();
+        alert('Viesti lähetetty onnistuneesti!');
+    });
+});
+
+window.addEventListener('scroll', function() {
+    var navbar = document.querySelector('.navbar');
+    if(window.scrollY > 100) {
+      navbar.classList.add('navbar-expand-md');
+    } else {
+      navbar.classList.remove('navbar-expand-md');
+    }
+  });
